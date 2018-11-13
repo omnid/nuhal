5f1e59e2503a40932bc7b94cff0fbc8a39b78664
Matthew Elwin <elwin@u.northwestern.edu>
2018-11-13 16:45:23 -0600

    Defined some delta robot parameters in cmake isntead of .h
    This is perhaps a bit unorthodox, but it will allow me to generate both
    a .h file from cmake with the proper parameters and the urdf from the
    xacro file at compile time.

---
+++ include/common/parameters.cmake
@@ -0,0 +1,12 @@
+# CMAKE file used for defining some parameters in common/include/parameters.h.in
+# see common/include/parameters.h.in for details.
+# Variables defined here can also be passed to xacro files during configure time
+set(PARAMETERS_DELTA_BASE_RADIUS 0.18f)
+set(PARAMETERS_DELTA_BASE_THICKNESS 0.009525f)
+set(PARAMETERS_DELTA_BASE_MASS 2.5)
+set(PARAMETERS_DELTA_BASE_OFFSET 0.046375)
+set(PARAMETERS_DELTA_PLATFORM_RADIUS  0.062f)
+set(PARAMETERS_DELTA_PLATFORM_THICKNESS 0.01f)
+set(PARAMETERS_DELTA_PLATFORM_MASS 0.5f)
+set(PARAMETERS_DELTA_LOWER_LEG_LENGTH 0.200f)
+set(PARAMETERS_DELTA_UPPER_LEG_LENGTH 0.368f)
