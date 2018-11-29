c3355f10cfec6f740b78985a2e4666d57a8d1bbb
Matthew Elwin <elwin@u.northwestern.edu>
2018-11-29 11:29:11 -0600

    Fixing build dependency problem when generating parameter.h
    Needed to make cmake target commond epend on the generation of parameter.h

--- include/common/parameters.cmake
+++ include/common/parameters.cmake
@@ -1,3 +1,6 @@
+# This file should be invoked from a custom_command so that the parameters.h
+# file is generated at build time
+
 # CMAKE file used for defining some parameters in common/include/parameters.h.in
 # see common/include/parameters.h.in for details.
 # Variables defined here can also be passed to xacro files during configure time
@@ -10,3 +13,9 @@ set(DELTA_PLATFORM_THICKNESS 0.01)
 set(DELTA_PLATFORM_MASS 0.5)
 set(DELTA_LOWER_LEG_LENGTH 0.200)
 set(DELTA_UPPER_LEG_LENGTH 0.368)
+
+# note parameters.h is placed in the build tree so we add include directories
+# for it in the build interface. THE PARAMETERS_INFILE and OUTFILE arguments
+# should be passed to the script
+#e.g. cmake -DPARAMETERS_IN_FILE=<fname> -DPARAMETERS_OUT_FILE=<fname> -P parameters.cmake
+configure_file(${PARAMETERS_INFILE} ${PARAMETERS_OUTFILE})
