2f8780dcb1d66315f61ea654b5ea8e0e70115198
Matthew Elwin <elwin@northwestern.edu>
2020-07-26 13:37:28 -0500

    Updated comments and description.

--- nuhal_linux/include/nuhal/uart_linux.h
+++ nuhal_linux/include/nuhal/uart_linux.h
@@ -1,14 +1,15 @@
-#ifndef NUTILITIES_X64_LINUX_UART_HOST_INCLUDE_GUARD
-#define NUTILITIES_X64_LINUX_UART_HOST_INCLUDE_GUARD
+#ifndef NUHAL_UART_LINUX_INCLUDE_GUARD
+#define NUHAL_UART_LINUX_INCLUDE_GUARD
 /// @file
-/// @brief host-specific uart functions
+/// @brief linux-specific uart functions
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /// @brief acquire a lock on the uart.  no other program
-/// may access the uart while the lock is held.
+/// should access the uart while the lock is held.
+///  This is used to allow multiple programs to access the same uart without interfering
 /// Useful to mediate multiple programs accessing the uart
 /// simultaneously. uart_lock blocks until the lock is acquired
 /// locking is cooperative, so a program that does not acquire the lock can still access the port
