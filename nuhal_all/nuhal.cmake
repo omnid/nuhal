f2d8f27d101c535840bb71266b8c4efd7b4218ed
Matthew Elwin <elwin@northwestern.edu>
2020-07-30 21:11:33 -0500

    Working on makign each platform independent of each other.

---
+++ nuhal_all/nuhal.cmake
@@ -0,0 +1,23 @@
+# set a default build type if not specified and store it in the cache
+# type - the build type to set
+function(nuhal_set_default_build_type type)
+  set(DEFAULT_BUILD_TYPE ${type})
+  if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE ${DEFAULT_BUILD_TYPE} CACHE
+      STRING "Type of build." FORCE)
+
+    message(STATUS "CMAKE_BUILD_TYPE=\"\": Defaulting to ${DEFAULT_BUILD_TYPE}")
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+      "Debug" "Release" "RelWithDebInfo" "MinSizeRel")
+  endif()
+endfunction()
+
+# disable in source builds
+function(nuhal_disable_in_source_builds)
+  if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
+    message(FATAL_ERROR
+      "No in source builds allowed. Create a separate build directory.
+       SOURCE_DIR=${CMAKE_SOURCE_DIR}  BINARY_DIR=${CMAKE_BINARY_DIR} ")
+  endif()
+endfunction()
+
